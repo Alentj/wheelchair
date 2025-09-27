a1402bf535c5c953e597d0779653b0ad8e3f8cf3
Alentj <mail2alentj@gmail.com>
2025-09-27 14:48:40 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 const firebaseConfig = {
   apiKey: "AIzaS*********************",
   authDomain: "smart-wheel-chair-8574a.firebaseapp.com",
-  projectId: "smart-wheel-chair-8574a",
+  projectId: "smar***********74a",
   storageBucket: "smart-wheel-chair-8574a.firebasestorage.app",
-  messagingSenderId: "841133598939",
-  appId: "1:841133598939:web:18b3a941b19f7147aa84fe"
+  messagingSenderId: "84********39",
+  appId: "1:84*********9:web:18b3**********87aa84fe"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
+
